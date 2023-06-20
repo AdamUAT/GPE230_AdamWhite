84d4b56cea5eb9aaf7374cbf794b48fbc0c9ef9b
AdamUAT <adawhite@uat.edu>
2023-06-20 11:47:53 -0700

    Made health component

---
+++ Source/GPE230_AdamWhite/Health.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Health.h"
+
+// Sets default values for this component's properties
+UHealth::UHealth()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+// Called when the game starts
+void UHealth::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Sets the value of currentHealth to maxHealth.
+	currentHealth = maxHealth;
+}
+
+void UHealth::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	if (DamageAmount > 0)
+	{
+		currentHealth -= DamageAmount;
+
+		if (currentHealth <= 0)
+		{
+			Die();
+		}
+	}
+}
+
+void UHealth::Die()
+{
+	//Do die stuff later.
+}
+
